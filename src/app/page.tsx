277a2c1f382736617b7b66834aca8e3326e7f610
maisylattanavong <maisylattanavong@gmail.com>
2024-09-20 08:56:52 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react'
 import React from 'react'
 
 export default function HmePage() {
   return (
-    <div>HmePage</div>
+    <div>
+      <Button disabled>
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </Button>
+    </div>
   )
 }
